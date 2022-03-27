8f3723c11e2556219579c4bcbe6c344de9781ca1
Dmitriy Kozlov <mr.dimk@icloud.com>
2022-03-27 19:26:44 +0300

    Добавил модуль работы с формой, функции активации и деактивации

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 import {createCards} from './cards.js';
+import {deactivateForm} from './form.js';
 
+deactivateForm();
 const mapCanvas = document.querySelector('#map-canvas');
 mapCanvas.appendChild(createCards(1));
