72fc50a64db61b3c689257e467b30d14c1bbad7e
Dmitriy Kozlov <mr.dimk@icloud.com>
2022-04-03 19:03:57 +0300

    Добавил валиюдацию формы

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {createCards} from './cards.js';
-import {deactivateForm} from './form.js';
+import {deactivateForm, activateForm} from './form.js';
 
 deactivateForm();
+activateForm(); // временно оставляю включенной для тестирования
 const mapCanvas = document.querySelector('#map-canvas');
 mapCanvas.appendChild(createCards(1));
